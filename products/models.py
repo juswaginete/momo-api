d014bb5dc6cad5c4483834da2c2eda9ecc27cb6b
kyle <kyle.naranjo@gmail.com>
2022-01-13 16:21:53 +0800

    fixed some fields in producttype model

--- products/models.py
+++ products/models.py
@@ -6,10 +6,13 @@ optional = {
 }
 
 class ProductType(models.Model):
-	name = models.CharField(max_length=30)
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_updated = models.DateTimeField(auto_now=True)
+    product_type_name = models.CharField(max_length=30, **optional)
 
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=False, **optional)
+    date_updated = models.DateTimeField(auto_now=False, auto_now_add=False, **optional)
+
+    def __str__(self):
+        return(self.product_type_name)
 
 class Products(models.Model):
     product_name = models.CharField(max_length=255, **optional)
