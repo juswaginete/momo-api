4236a5cf3f5a2c55dab0e44c54636616a930a03d
Johann <johannkristoff.naranjo@gmail.com>
2022-01-14 23:57:54 +0800

    add endpoints for product type

--- products/urls_product_types.py
+++ products/urls_product_types.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from .views import ProductTypesView
 
 urlpatterns = [
-    path('', ProductTypesView.as_view(), name="product_types")
+    path('', ProductTypesView.as_view(), name="product-types")
 ]
